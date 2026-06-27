9cd045109c32d0813c2064e724954c3fe179e52b
H Nitesh Kumar <nitesh6355555@gmail.com>
2026-06-27 17:53:08 +0530

    Update mydata_template.h

--- main/mydata_template.h
+++ main/mydata_template.h
@@ -11,4 +11,13 @@
 #define MQTT_TOPIC_VERSION     "esp32/device1/version"
 #define MQTT_TOPIC_TEMPERATURE "esp32/device1/temperature"
 
+#define PWM_GPIO GPIO_NUM_2
+#define OTA_LED_GPIO GPIO_NUM_17
+
+//#define FW_VERSION "v1.0.1"
+//#define FW_VERSION "v1.0.2"
+//#define FW_VERSION "v1.0.3"
+#define FW_VERSION "v1.0.4"
+
+
 #endif

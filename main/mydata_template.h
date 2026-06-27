bc5a4603e30549a823520419b71c6ade611738da
H Nitesh Kumar <nitesh6355555@gmail.com>
2026-06-27 17:38:56 +0530

    Create mydata_template.h

---
+++ main/mydata_template.h
@@ -0,0 +1,14 @@
+#ifndef MYDATA_H
+#define MYDATA_H
+
+#define MQTT_URI  "mqtts://your-server.s1.eu.hivemq.cloud:8883"
+#define MQTT_USER "YOUR_USERNAME"
+#define MQTT_PASS "YOUR_PASSWORD"
+
+#define MQTT_TOPIC_STATUS      "esp32/device1/status"
+#define MQTT_TOPIC_CMD         "esp32/device1/led"
+#define MQTT_TOPIC_OTA         "esp32/device1/ota"
+#define MQTT_TOPIC_VERSION     "esp32/device1/version"
+#define MQTT_TOPIC_TEMPERATURE "esp32/device1/temperature"
+
+#endif
